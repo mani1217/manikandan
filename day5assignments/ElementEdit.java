10d5571c3afbc3edf0a62720bf7b6d60fb5b606a
mani1217 <104966584+mani1217@users.noreply.github.com>
2022-05-24 23:02:35 +0530

    Add files via upload

---
+++ day5assignments/ElementEdit.java
@@ -0,0 +1,43 @@
+package week2.day5assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ElementEdit {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	ChromeDriver driver = new ChromeDriver();
+	driver.get("http://www.leafground.com/pages/Edit.html");
+	driver.manage().window().maximize();
+	
+	//ENTER YOUR EMAIL ADDRESS
+	
+	WebElement findElement = driver.findElement(By.xpath("//label[contains(text(),'Enter your email address')]/following-sibling::input"));
+	findElement.sendKeys("JACKDANIELS@123.COM");
+	
+	//APPEND A TEXT
+	
+	WebElement findElement2 = driver.findElement(By.xpath("//label[contains(text(),'Append a text and press keyboard tab')]/following-sibling::input"));
+	findElement2.sendKeys("vera mari");
+	
+	//GET DEFAULT TEXT
+	
+	WebElement findElement3 = driver.findElement(By.xpath("//label[contains(text(),'Get default text entered')]/following-sibling::input"));
+	String attribute = findElement3.getAttribute("value");
+	System.out.println(attribute);
+	
+	//CLEAR ME
+	
+	WebElement findElement4 = driver.findElement(By.xpath("//label[contains(text(),'Clear the text')]/following-sibling::input"));
+	findElement4.clear();
+	
+	//THAT FIELD IS DISABLED
+	
+	WebElement findElement5 = driver.findElement(By.xpath("//label[contains(text(),'Confirm that edit field is disabled')]/following-sibling::input"));
+	boolean enabled = findElement5.isEnabled();
+	System.out.println(enabled);
+}
+}
